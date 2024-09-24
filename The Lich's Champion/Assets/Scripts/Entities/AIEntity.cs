fdf02751999c17d7943a3a82a9a7be24a1019d18
yaboiedchoi <ec2244@rit.edu>
2024-09-24 02:03:07 -0400

    added simple player prefab and network manager
    network manager currently not working

--- The Lich's Champion/Assets/Scripts/Entities/AIEntity.cs
+++ The Lich's Champion/Assets/Scripts/Entities/AIEntity.cs
@@ -1,9 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class AIEntity : Entity
 {
+    // all entity stats
+    private int health;
+    private int maxHealth;
+    private int damage;
+    private float speed;
+    private float healthRegen;
+    private int energy;
+    private int maxEnergy;
+    private float energyRegen;
+
+    // properties
+    #region player stat properties
+
+    public override int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public override int MaxHealth
+    {
+        get { return maxHealth; }
+        set { maxHealth = value; }
+    }
+
+    public override int Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    public override float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    public override float HealthRegen
+    {
+        get { return healthRegen; }
+        set { healthRegen = value; }
+    }
+
+    public override int Energy
+    {
+        get { return energy; }
+        set { energy = value; }
+    }
+
+    public override int MaxEnergy
+    {
+        get { return maxEnergy; }
+        set { maxEnergy = value; }
+    }
+
+    public override float EnergyRegen
+    {
+        get { return energyRegen; }
+        set { energyRegen = value; }
+    }
+
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
--- The Lich's Champion/Assets/Scripts/Entities/PlayerEntity.cs
+++ The Lich's Champion/Assets/Scripts/Entities/PlayerEntity.cs
@@ -1,18 +1,153 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerEntity : Entity
 {
+    // all entity stats
+    private int health;
+    private int maxHealth;
+    private int damage;
+    private float speed = 100;
+    private float healthRegen;
+    private int energy;
+    private int maxEnergy;
+    private float energyRegen;
+
+    // additional fields
+
+    // movement
+
+    [SerializeField]
+    private Vector3 position;
+    [SerializeField]
+    private Vector3 velocity;
+    [SerializeField]
+    private Vector3 direction;
+    [SerializeField]
+    private Vector3 acceleration;
+    [SerializeField]
+    private float mass = 1;
+    [SerializeField]
+    private float maxSpeed = 10;
+    [SerializeField]
+    private float frictionCoeff = 0.1f;
+
+    // player exclusive stats
+
+    // levels?
+    // experience?
+    // gold?
+
+    // properties
+    #region player stat properties
+
+    public override int Health 
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public override int MaxHealth 
+    {
+        get { return maxHealth; }
+        set { maxHealth = value; }
+    }
+
+    public override int Damage 
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    public override float Speed 
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    public override float HealthRegen 
+    {
+        get { return healthRegen; }
+        set { healthRegen = value; }
+    }
+
+    public override int Energy 
+    {
+        get { return energy; }
+        set { energy = value; }
+    }
+
+    public override int MaxEnergy 
+    {
+        get { return maxEnergy; }
+        set { maxEnergy = value; }
+    }
+
+    public override float EnergyRegen 
+    {
+        get { return energyRegen; }
+        set { energyRegen = value; }
+    }
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // friction
+        Vector3 friction = velocity * -1;
+        friction.Normalize();
+        friction = friction * frictionCoeff;
+        acceleration += friction / mass;
+
+
+        SimpleMovement();
+
+        velocity += acceleration * Time.deltaTime;
+
+        // clamp the velocity to the max speed
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        position += velocity * Time.deltaTime;
+        direction = velocity.normalized;
+
+        transform.position = position;
+        acceleration = Vector3.zero;
+
+        // rotate the player to face the direction they are moving
+        if (velocity != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
+            transform.rotation = rotation;
+        }
+    }
+
+    public void SimpleMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            acceleration += Vector3.right * speed * Time.deltaTime * 1000;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            acceleration += Vector3.left * speed * Time.deltaTime * 1000;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            acceleration += Vector3.up * speed * Time.deltaTime * 1000;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            acceleration += Vector3.down * speed * Time.deltaTime * 1000;
+        }
     }
 }
