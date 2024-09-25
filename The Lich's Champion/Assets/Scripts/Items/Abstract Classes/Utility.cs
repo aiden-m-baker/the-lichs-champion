c5296e853230504605f394f616e925ecbdd1d869
lmfaono <amb6039@rit.edu>
2024-09-25 13:28:23 -0400

    Created utility and weapon prefabs (keep forgetting to push mb)

--- The Lich's Champion/Assets/Scripts/Items/Abstract Classes/Utility.cs
+++ The Lich's Champion/Assets/Scripts/Items/Abstract Classes/Utility.cs
@@ -6,6 +6,8 @@ public abstract class Utility : Item
 {
     [SerializeField][Min(0)] protected float cooldown_ActionNormal;
     [SerializeField][Min(0)] protected float cooldown_ActionSpecial;
+    protected float cooldownTracker_ActionNormal;
+    protected float cooldownTracker_ActionSpecial;
 
     /// <summary>
     /// Function called when player inputs normal action
@@ -16,4 +18,18 @@ public abstract class Utility : Item
     /// Function called when player inputs specialized action. Can do nothing if specific item does not have a special action
     /// </summary>
     public abstract void ActionSpecial();
+
+    protected virtual void LateUpdate()
+    {
+        // If cooldowns are less than 0, reset tracker to 0. Else, count down timer
+        cooldownTracker_ActionNormal = cooldownTracker_ActionNormal < 0 ? 0 : cooldownTracker_ActionNormal - Time.deltaTime;
+        cooldownTracker_ActionSpecial = cooldownTracker_ActionSpecial < 0 ? 0 : cooldownTracker_ActionSpecial - Time.deltaTime;
+    }
+
+    protected virtual void ResetAction()
+    {
+        StopAllCoroutines();
+        cooldownTracker_ActionNormal = 0;
+        cooldownTracker_ActionSpecial = 0;
+    }
 }
--- The Lich's Champion/Assets/Scripts/Items/Sword.cs
+++ The Lich's Champion/Assets/Scripts/Items/Sword.cs
@@ -4,19 +4,74 @@ using UnityEngine;
 
 public class Sword : Weapon
 {
+    [SerializeField] private Vector3 defaultRotation;
+
     private void Awake()
     {
-        cooldown_ActionNormal = 0.25f;
-        cooldown_ActionSpecial = 0.75f;
+        name = "Sword";
+        rarity = Rarity.Common;
+        if (!spriteObject)
+            spriteObject = transform.Find("SpriteObject").gameObject;
+        if (!hitbox)
+            hitbox = transform.Find("HitboxObject").GetComponent<Collider2D>();
+
+        spriteObject.GetComponent<SpriteRenderer>().sprite = sprite;
+
+        ResetAction();
     }
 
+    /// <summary>
+    /// Swing sword in front of player,
+    /// </summary>
     public override void ActionNormal()
     {
+        if (cooldownTracker_ActionNormal > 0) return;
 
+        // Set and start countdown
+        cooldownTracker_ActionNormal = cooldown_ActionNormal;
+
+        print("Attacking Normal!");
+
+        ResetAction();
+
+        StartCoroutine(SwingSwordSprite());
     }
 
+    /// <summary>
+    /// Grab, stab, and push back enemy player
+    /// </summary>
     public override void ActionSpecial()
     {
+        if (cooldownTracker_ActionSpecial > 0) return;
 
+        // Set and start countdown
+        cooldownTracker_ActionSpecial = cooldown_ActionSpecial;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+            ActionNormal();
+    }
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+
+    protected override void ResetAction()
+    {
+        base.ResetAction();
+
+        spriteObject.transform.rotation = Quaternion.Euler(defaultRotation);
+    }
+
+    private IEnumerator SwingSwordSprite()
+    {
+        Vector3 rot = Vector3.zero;
+        rot.z = 10;
+        spriteObject.transform.rotation = Quaternion.Euler(rot);
+
+        yield return null;
     }
 }
