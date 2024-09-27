92ac751b135e0dad283c3bba8a494610a2069f6b
Bin Xu (RIT Student) <bx2260@rit.edu>
2024-09-27 12:32:37 -0400

    add player controller

--- The Lich's Champion/Assets/Scripts/SimpleMovement.cs
+++ The Lich's Champion/Assets/Scripts/SimpleMovement.cs
@@ -4,19 +4,8 @@ public class SimpleMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
-    private Vector3 _movement = Vector3.zero;
-
-    void Update()
+    public void AddMovement(Vector3 direction)
     {
-        if (_movement.magnitude > 0)
-        {
-            transform.Translate(_movement.normalized * (moveSpeed * Time.deltaTime));
-            _movement = Vector3.zero;
-        }
-    }
-
-    void AddMovement(Vector3 movement)
-    {
-        _movement = movement;
+        transform.Translate(direction.normalized * (moveSpeed * Time.deltaTime));
     }
 }
