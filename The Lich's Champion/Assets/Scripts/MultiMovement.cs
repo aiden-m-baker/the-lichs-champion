49c08833d5dbe50ca6b3a3405457196d4cbba1ae
yaboiedchoi <ec2244@rit.edu>
2024-10-18 00:58:48 -0400

    added dash for controller and kbm

--- The Lich's Champion/Assets/Scripts/MultiMovement.cs
+++ The Lich's Champion/Assets/Scripts/MultiMovement.cs
@@ -23,18 +23,35 @@ public class MultiMovement : MonoBehaviour
     private Vector2 movementInput;
 
     private Vector2 aimInput;
+    private Vector2 aimInputMouse;
     private Vector2 previousAimInput;
     
-    [SerializeField] private float moveSpeed = 2000f;
+    private float moveSpeed = 40f;
 
     // dash contains whether or not the button is pressed
     // dashing contains whether or not you are currently dashing
+    // dashmax is the time spent dashing
+    // dashtimer is a helper variable to count said time
     [SerializeField]
-    private bool dash, dashing;
+    private bool dashing;
+    [SerializeField]
+    private float dashPressed;
+
+    private float dashSpeed = 35f;
+
+    [SerializeField]
+    private float dashTimer = 0f;
+    [SerializeField]
+    private float dashMax = 0.15f;
     // dashCdMax is the total dash cooldown
     // dashCdTimer is a helper variable to count said cooldown
+    [SerializeField]
     private float dashCdMax = 5f;
+    [SerializeField]
     private float dashCdTimer = 0f;
+
+    // hold previous dash location
+    Vector2 dashLocation;
 
     [SerializeField]
     private Vector3 position, velocity, direction, acceleration;
@@ -56,29 +73,89 @@ public class MultiMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //Debug.Log(moveSpeed);
+        //Debug.Log(_rb.velocity);
+        //Debug.Log(dashPressed);
         //Debug.Log(playerInput.currentControlScheme);
         // TODO: Add a deadzone to the movement input, so letting go of the joystick doesnt flick you
         // old non-physics input
         //transform.position += (Vector3)movementInput * Time.deltaTime * speed;
-        
-        _rb.AddForce(movementInput * (moveSpeed * Time.deltaTime));
 
-        //// look towards your aim stick orientation (or previous orientation)
-        //if (CurrentControlScheme == "MouseKeyboard")
-        //{
-        //    aimInput = mainCam.ScreenToWorldPoint(aimInput) - transform.position;
-        //}
-        // save previous rotation
+        // restore previous rotation if no input
         if (aimInput.magnitude == 0 && CurrentControlScheme != "MouseKeyboard")
         {
             aimInput = previousAimInput;
         }
 
-        // look towards your aim stick orientation (or previous orientation)
-        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, aimInput.normalized);
-        transform.rotation = rotation;
+        // if not dashing, allow movement
+        if (!dashing)
+            _rb.AddForce(movementInput * moveSpeed);
+
+        // if dashing, count down the timer
+        if (dashing)
+        {
+            if (aimInput.magnitude != 0)
+            {
+                //_rb.AddForce(dashLocation * dashSpeed);
+            }
+            else
+            {
+                //_rb.AddForce((dashLocation - (Vector2)transform.position).normalized * dashSpeed);
+            }
+        }
+        // if not dashing, set dashing to false
+        if (dashTimer <= 0)
+        {
+            dashing = false;
+        }
+
+        // when key is pressed, and you are not currently dashing, and the cd is done
+        if (dashPressed == 1 && !dashing && dashCdTimer <= 0)
+        {
+            Debug.Log("DASH BUT ACTUALLY");
+            dashing = true;
+            // reset timers
+            dashCdTimer = dashCdMax;
+            dashTimer = dashMax;
+            _rb.velocity = Vector3.zero;
+
+            if (aimInput.magnitude != 0)
+            {
+                dashLocation = aimInput.normalized;
+                _rb.AddForce(aimInput.normalized * dashSpeed, ForceMode2D.Impulse);
+            }
+            else
+            {
+                dashLocation = aimInputMouse.normalized;
+                _rb.AddForce((aimInputMouse.normalized - (Vector2)transform.position).normalized * dashSpeed, ForceMode2D.Impulse);
+            }
+        }
+
+
+        // count cooldowns
+        UpdateTimers();
+
+        // TODO: Fix Mouse Player Rotation
+        //// look towards your aim stick orientation (or previous orientation)
+        //if (CurrentControlScheme == "MouseKeyboard")
+        //{
+        //    aimInput = mainCam.ScreenToWorldPoint(aimInput) - transform.position;
+        //}
+        
+        if (aimInput.magnitude != 0)
+        {
+            // look towards your aim stick orientation (or previous orientation)
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, aimInput.normalized);
+            transform.rotation = rotation;
+        }
+        else
+        {
+            // look towards your aim stick orientation (or previous orientation)
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, aimInputMouse - (Vector2)transform.position);
+            transform.rotation = rotation;
+        }
 
         // save previous input
         if (aimInput.magnitude > 0)
@@ -91,12 +168,21 @@ public class MultiMovement : MonoBehaviour
     }
 
     public void OnAim(InputAction.CallbackContext ctx) => aimInput = ctx.ReadValue<Vector2>();
-    public void OnDash(InputAction.CallbackContext ctx) => dash = ctx.ReadValueAsButton();
+    public void OnAimMouse(InputAction.CallbackContext ctx) => aimInputMouse = mainCam.ScreenToWorldPoint(ctx.ReadValue<Vector2>());
+    public void OnDash(InputAction.CallbackContext ctx) => dashPressed = ctx.ReadValue<float>();
     public void ApplyForce(Vector3 direction)
     {
         // if OnMove is already normalized, then pass in the original vector2. 
         // this gives the player more control on the movement. not available for kb+m
         acceleration += (direction * speed * Time.deltaTime) / mass;
+    }
+
+    private void UpdateTimers()
+    {
+        if (dashTimer >= 0)
+            dashTimer -= Time.fixedDeltaTime;
+        if (dashCdTimer >= 0)
+            dashCdTimer -= Time.fixedDeltaTime;
     }
 }
 
