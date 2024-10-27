b07642cd017e625d70ef8cd255dcd43d09730541
yaboiedchoi <ec2244@rit.edu>
2024-10-27 19:17:53 -0400

    multiplayer movement working. TODO: Fix weapon pickup and swinging in multiplayer

---
+++ The_Lich's_Champion_clone_0/Assets/Scripts/Entities/TargetDummy.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+
+public class TargetDummy : Entity
+{
+    // all entity stats
+    private int health;
+    private int maxHealth = 100;
+    private int damage = 0;
+    private float speed = 0;
+    private float healthRegen;
+    private int energy;
+    private int maxEnergy;
+    private float energyRegen;
+
+    // player IFrames
+    public float iFrameDuration = 0.5f;
+    public float iFrameTimer = 0f;
+
+    // properties
+    #region player stat properties
+
+    public override int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public override int MaxHealth
+    {
+        get { return maxHealth; }
+        set { maxHealth = value; }
+    }
+
+    public override int Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    public override float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    public override float HealthRegen
+    {
+        get { return healthRegen; }
+        set { healthRegen = value; }
+    }
+
+    public override int Energy
+    {
+        get { return energy; }
+        set { energy = value; }
+    }
+
+    public override int MaxEnergy
+    {
+        get { return maxEnergy; }
+        set { maxEnergy = value; }
+    }
+
+    public override float EnergyRegen
+    {
+        get { return energyRegen; }
+        set { energyRegen = value; }
+    }
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        // knockback test code
+        // TakeDamage(10, new Vector3(0, -1, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // count IFrame cooldown
+        if (iFrameTimer > 0)
+        {
+            iFrameTimer -= Time.deltaTime;
+        }
+
+    }
+    public override void TakeDamage(int damage, Vector3 sourceLoc)
+    {
+        if (iFrameTimer <= 0)
+        {
+            health -= damage;
+            iFrameTimer = iFrameDuration;
+
+            if (sourceLoc != null)
+            {
+                // 0.1f == knockback constant
+                transform.position += (transform.position - sourceLoc).normalized * 0.1f;
+            }
+        }
+    }
+    public override void ApplyStatusEffect(StatusEffect effect)
+    {
+        throw new NotImplementedException();
+    }
+}
