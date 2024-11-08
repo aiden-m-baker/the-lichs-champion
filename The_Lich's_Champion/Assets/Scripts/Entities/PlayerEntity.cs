a1f758aaddf087f1b34dbd39493145fc767e042e
Aiden Baker <aiden.baker623@gmail.com>
2024-11-08 11:29:39 -0800

    fixed a bug where the player would kill itself on spawn

--- The_Lich's_Champion/Assets/Scripts/Entities/PlayerEntity.cs
+++ The_Lich's_Champion/Assets/Scripts/Entities/PlayerEntity.cs
@@ -176,7 +176,7 @@ public class PlayerEntity : Entity
         #endregion
 
         // check if player dead
-        if (true)
+        if (IsDead)
         {
             Destroy(gameObject);
         }
